c8ebb6ed41489db3caa12fec994345b0fac2f995
Ash1shh <Ashishchandra8328@gmail.com>
2023-03-07 12:40:50 +0530

    :rocket: Twitter-Clone

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import { getProviders, signIn as SignIntoProvider } from 'next-auth/react';
+import React from 'react';
+import { getProviders, signIn as SignIntoProvider, ClientSafeProvider } from 'next-auth/react';
 import { InferGetServerSidePropsType } from 'next';
 import Head from 'next/head';
 
 function signIn({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     return (
-
         <>
             <Head>
                 <title>Twitter Clone</title>
                 <link rel="icon" href="https://i0.wp.com/edinburghuniform.org/wp-content/uploads/2019/11/twitter-logo-png-twitter-logo-vector-png-clipart-library-518.png?fit=518%2C518&ssl=1" />
             </Head>
-            
             <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center pt-60">
                 <img
                     //className="h-120"
@@ -23,29 +21,25 @@ function signIn({ providers }: InferGetServerSidePropsType<typeof getServerSideP
                     This is not a Real app, it is build for educational purpose only
                 </p>
                 <div className='mt-40'>
-                    {Object.values(providers).map((provider) => (
+                    {providers && Object.values(providers).map((provider) => (
                         <div key={provider.name}>
-                            <button
-                                className="p-3 bg-twitter rounded-lg text-white" onClick={() => SignIntoProvider(provider.id, { callbackUrl: "/" })}>
+                            <button className="p-3 bg-twitter rounded-lg text-white" onClick={() => SignIntoProvider(provider.id, { callbackUrl: "/" })}>
                                 Sign in with {provider.name}
                             </button>
                         </div>
                     ))}
                 </div>
             </div>
-
-
         </>
     )
 }
 
 export async function getServerSideProps() {
     const providers = await getProviders();
-
     return {
         props: {
-            providers
-        }
+            providers,
+        },
     }
 }
 
